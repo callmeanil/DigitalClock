56920622d268e00576748f472f1696602d017a38
Anil Kumar <82893113+callmeanil@users.noreply.github.com>
2023-09-04 01:58:59 +0530

    Add files via upload

---
+++ newProject/index.js
@@ -0,0 +1,30 @@
+const hourEle = document.getElementById("hour");
+const minuteEle = document.getElementById("minutes");
+const secondEle = document.getElementById("second");
+const ampmEle = document.getElementById("ampm");
+
+function updateClock() {
+  let h = new Date().getHours();
+  let m = new Date().getMinutes();
+  let s = new Date().getSeconds();
+  let ampm = "AM";
+
+  if (h > 12) {
+    h = h - 12;
+    ampm = "PM";
+  }
+
+  h = h < 10 ? "0" + h : h;
+  m = m < 10 ? "0" + m : m;
+  s = s < 10 ? "0" + s : s;
+
+  hourEle.innerText = h;
+  minuteEle.innerText = m;
+  secondEle.innerText = s;
+  ampmEle.innerText = ampm;
+
+  setTimeout(() => {
+    updateClock()
+  }, 1000)
+}
+updateClock();
